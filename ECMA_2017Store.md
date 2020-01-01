d4e242483ef3b034d20d1471f53c4ffd88b1215d
iWuzhi <Kaer_600776@163.com>
2020-01-01 09:35:00 +0800

    update

---
+++ ECMA_2017Store.md
@@ -0,0 +1,8 @@
+# Object
+  1. Object.entries
+2. Object.getOwnPropertyDescriptors
+3. Object.values
+  # String
+  1. String.padEnd
+2. String.padStart
+  
--- revertJSONToMarkdown.js
+++ revertJSONToMarkdown.js
@@ -10,11 +10,35 @@ const ECMA_2019Store = 'ECMA_2019Store';
 const ECMA_2020Store = 'ECMA_2020Store';
 
 function revert(jsonPath) {
-  fs.readJson(`./${jsonPath}.json`, (err, packageObj) => {
+  fs.readJson(`./${jsonPath}.json`, (err, esVersionInfo) => {
     if (err) {
       console.error(err);
       return;
     }
-    console.log(packageObj.version) // => 0.1.3
+    let targetMD = '';
+    for (let esVersionInfoItemKey in esVersionInfo) {
+      let esVersionInfoItems = esVersionInfo[esVersionInfoItemKey];
+      if (!esVersionInfoItems || esVersionInfoItems.length === 0) continue;
+      targetMD += moudleTemplate(esVersionInfoItemKey, esVersionInfoItems);
+    }
+    fs.outputFile(`./${jsonPath}.md`, targetMD, function (err) {
+      if (err) console.error(err)
+    })
   })
 }
+
+function moudleTemplate(title, subKeys) {
+  let target = `# ${title}
+  ${subKeys.map(generateSubLine).join('\n')}
+  `;
+  function generateSubLine(subKey, index) {
+    return `${index + 1}. ${title}.${subKey}`;
+  }
+  return target;
+}
+revert(ECMA_2015Store);
+revert(ECMA_2016Store);
+revert(ECMA_2017Store);
+revert(ECMA_2018Store);
+revert(ECMA_2019Store);
+revert(ECMA_2020Store);
